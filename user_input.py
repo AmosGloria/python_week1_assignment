d317637aa432c918446db1bad16525df7845bf76
AmosGloria <amoskesegloria@gmail.com>
2024-08-14 21:32:23 +0100

    feat: added user_input.py file

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask for the user's name
+name = input("What is your name? ")
+
+# Ask for the user's age
+age = input("How old are you? ")
+
+# Ask for the user's location
+location = input("Where are you located? ")
+
+# Print out a personalized message
+print(f"Hello, {name}! You are {age} years old and live in {location}. Nice to meet you!")
